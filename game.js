54630f7e2d35e1c98a13635f858146a7ceab32b3
TheBigKei <TheBigKei@yandex.ru>
2024-04-10 07:49:06 +0300

    commit1

--- game.js
+++ game.js
@@ -1,70 +1,75 @@
-const counter = createCounter;
+const counter = createCounter();
+
+var rndNumber = Math.floor(Math.random() * 101);
 
 document.getElementById("formInput").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    var guesses = document.querySelector('#guesses')
-    var lastResult = document.querySelector('')
-
     var inputNumber = document.getElementById("nInput").value;
-    var rndNumber = Math.floor(Math.random() * 101);
+    if (inputNumber===''){
+        inputNumber = 0;
+    }
+
     var answerStatus = ['Congratulations! You got it right!','Last guess was too high!','Last guess was too low!']
 
-    switch(inputNumber){
-        case inputNumber === rndNumber:
-            print(answerStatus[0])
-            break;
-
-        case inputNumber > rndNumber:
-            print(answerStatus[1])
-            break;
-
-        case inputNumber < rndNumber:
-            print(answerStatus[2])
-            break;
+    if(inputNumber>rndNumber){
+        print(inputNumber,answerStatus[1]);
+    }
+    else if(inputNumber<rndNumber){
+        print(inputNumber,answerStatus[2]);
+    }
+    else if(inputNumber===rndNumber){
+        print(inputNumber,answerStatus[0]);
+    }
+    else {
+        throw console.error();
     }
 
 });
-// 'Congratulations! You got it right!'
-function print (answerStatus){
 
-    p = document.querySelectorAll('[data-clear="true"]');          
-
+function print (inputNumber,answerStatus){
     let conditionsObj = {
         'Last guess was too high!': false,
-        'Last guess was too low!':false
+        'Last guess was too low!': false
     }
+
+    document.getElementById("previousGuesses").innerText += inputNumber + 'ㅤ';
 
     if (conditionsObj[answerStatus]===false){
-        document.getElementById("answerStatus").innerText = 'Wrong!'
+        document.getElementById("answerStatus").innerText = '\n' + answerStatus;
+        document.getElementById("guesses").innerText = 'Total guesses: ' + counter();
     }
     else{
-        document.getElementById("answerStatus").innerText = answerStatus
-        p.forEach(p => {
-                p.innerHTML = '';
-            });
+        document.getElementById("answerStatus").innerText = '\n' + answerStatus;
+        endGame(true);
     }
-
-    
 
 }
 
 function createCounter(){
-    let guessCount = 0;
-    function counter(result){
+    var guessCount = 0;
+
+    function counter(){
+
         guessCount++;
-        if(guessCount!==10){
-            return guessCount;
+
+        if(guessCount>=10){
+            endGame(false)
         }
-        else{
-            endGame(false);
-        }
+        return guessCount;
     }
+
     return counter;
 }
 
-function endGame(){
-    let count = 0;
-    
+function endGame(result){
+    if(result === false){
+        resetGame()
+    }
 
+}
+
+function resetGame(){
+    guessCount = 0;
+    console.log(guessCount)
 }
